0393bfc5c412b4dfbdfd68c6d90562383b461bea
c-garrett02 <cadegarrett2@gmail.com>
2025-10-10 11:50:12 -0400

    First (bad) solution to wednesday's prob

---
+++ 10-8-25/2300-1.ts
@@ -0,0 +1,36 @@
+function successfulPairs(spells: number[], potions: number[], success: number): number[] {
+
+    //Bad approach. Too much needless iteration.
+
+    let pairs: number[] = [];
+    potions.sort((a,b) => a-b);
+    let greaterThan: number[] = [];
+    let lastNum: number = potions[potions.length-1];
+    greaterThan[lastNum] = 1;
+
+    for (let i: number = potions.length-2; i >= 0; i--){
+        if(lastNum === potions[i]){
+            greaterThan[lastNum] += 1;
+        }
+        else{
+            for(let j: number = lastNum-1; j >= potions[i]; j--){
+                greaterThan[j] = greaterThan[lastNum];
+            }
+            lastNum = potions[i];
+            greaterThan[lastNum] += 1;
+        }
+    }
+
+    if(greaterThan[0] === undefined){
+        for(let i: number = lastNum-1; i > 0; i--){
+            greaterThan[i] = greaterThan[lastNum];
+        }
+    }
+
+    for (let i: number = 0; i < spells.length; i++){
+        let minPotion = Math.ceil(success/spells[i]);
+        pairs.push(greaterThan[minPotion] !== undefined ? greaterThan[minPotion] : 0);
+    }
+
+    return pairs;
+};
